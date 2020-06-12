005e48bd4976444449fb652b0133096d1973630a
Eric Fredine <eric.fredine@beanworks.com>
2020-06-12 16:44:24 -0700

    Adds interpreter.

--- src/main/java/com/fredine/lox/Lox.java
+++ src/main/java/com/fredine/lox/Lox.java
@@ -1,5 +1,7 @@
 package com.fredine.lox;
 
+import org.omg.CORBA.INTERNAL;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,7 +11,9 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox {
+    private static final Interpreter interpeter = new Interpreter();
     static boolean hadError = false;
+    static boolean hadRuntimeError = false;
 
     public static void main(String[] args) throws IOException {
         if (args.length > 1) {
@@ -26,6 +30,9 @@ public class Lox {
         run(new String(bytes, Charset.defaultCharset()));
         if (hadError) {
             System.exit(65);
+        }
+        if (hadRuntimeError) {
+            System.exit(70);
         }
     }
 
@@ -50,7 +57,8 @@ public class Lox {
         if (hadError) {
             return;
         }
-        System.out.println(new AstPrinter().print(expression));
+//        System.out.println(new AstPrinter().print(expression));
+        interpeter.interpret(expression);
     }
 
     static void error(int line, String message) {
@@ -63,6 +71,11 @@ public class Lox {
         } else {
             report(token.line, " at '" + token.lexeme + "'", message);
         }
+    }
+
+    static void runtimeError(RuntimeError error) {
+        System.err.println(error.getMessage() + "\n[line " + error.token.line + "]");
+        hadRuntimeError = true;
     }
 
     private static void report(int line, String where, String message) {
